a6431ce86fe153b4c9115fcabe145ac4d2f5a6c9
faken3m0 <90421550+faken3m0@users.noreply.github.com>
2024-03-11 01:42:32 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -75,34 +75,4 @@ $(function(){
 		}
 	});
 	
-	
-	/*=========================================================================
-		Testimonials Slider
-	=========================================================================*/
-	$('.testimonials-slider').owlCarousel({
-		items: 2,
-		responsive:{
-			992: {
-				items: 2
-			},
-			0: {
-				items: 1
-			}
-		}
-	});
-	
-	
-	if ($('.hero .hero-text h2').length == 1) {
-        var typed_strings = $('.hero .hero-text .typed-text').text();
-        var typed = new Typed('.hero .hero-text h2', {
-            strings: typed_strings.split(', '),
-            typeSpeed: 100,
-            backSpeed: 20,
-            smartBackspace: false,
-            loop: true
-        });
-    }
-	
-	
-	
 });
